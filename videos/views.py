d10b12dfafdf28c2d163f95cfa4584c64976ef68
cywinskikamil <cywinski.kamil@yahoo.com>
2016-04-19 23:33:35 +0200

    basic index page

--- videos/views.py
+++ videos/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def videos_list(request):
-    return HttpResponse("Videos:")
+    return render(request, 'index.html')
 
 
 def single_video(request, video_slug):
