d6370159b55135f7112532027d67c968a11112e2
mihal277 <mihal277@gmail.com>
2016-04-20 23:44:55 +0200

    Small addition to utils test

--- videos/tests/utils.py
+++ videos/tests/utils.py
@@ -7,13 +7,15 @@ class UtilsTestCase(TestCase):
     CORRECT_VIDEOS = [
         'https://www.youtube.com/watch?v=jdiAraeA_8k&t=638s',
         'https://www.youtube.com/watch?v=-iSxls2f0Qo',
+        'http://www.youtube.com/watch?v=-iSxls2f0Qo',
         'www.youtube.com/watch?v=-iSxls2f0Qo',
         'www.youtube.com/watch?v=-iSxls2f0Qo'
     ]
 
     INCORRECT_VIDEOS = [
         'https://www.youtube.com/watch?v=dsguhdiufgh_',
-        'https://www.google.com/'
+        'https://www.google.com/',
+        'sdjfnksjafnbubiubsadf'
     ]
 
     def test_random_url_populate(self):
