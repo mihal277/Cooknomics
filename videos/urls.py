ea911c1e793f5b3ef34947d8b17146b8c8c158ff
Michal Pryt <misi3kd@gmail.com>
2016-06-03 00:51:11 +0200

    Added views for product searching and JS+HTML elements to send requests and display results. Also linked Bootstrap + Bootstrap-select in base.html

--- videos/urls.py
+++ videos/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^vote$', views.vote, name='vote'),
     url(r'^page$', views.video_page, name='page'),
     url(r'^(?P<video_slug>([a-zA-Z0-9-])+)/$', views.single_video, name='single_video'),
+    ####### URLE DO WYSZUKIWANIA DLA MAJKELA ########################
+    url(r'^get_items$', views.get_items, name='get_items'),
+    url(r'^process_items', views.process_items, name='process_items'),
 ]
