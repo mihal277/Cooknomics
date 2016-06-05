20a8850734309ed7eb58f753ab4b8f7934fd73d3
mihal277 <mihal277@gmail.com>
2016-06-06 01:59:10 +0200

    Added JS function which trims article content

--- news/static/news/js/scripts.js
+++ news/static/news/js/scripts.js
@@ -1,11 +1,27 @@
+function trim_str_to_n(str, n) {
+    if (str.length > n) {
+        var white_space_index = str.substring(n).indexOf(' ');
+        if (!white_space_index)
+            return str;
+        return (str.substring(0, white_space_index) + "...");
+    }
+    else {
+        return str;
+    }
+}
+
 function appendElements(newData) {
     var newElementsHtml = [];
     $.each(newData, function(element, data) {
-        var htmlString = '<li>';
+        var htmlString = '<div class="article-list-single">';
 
         htmlString += '<a href="' + data.url + '">';
-        htmlString += '<h2 class="title">' + data.title + '</h2>';
-        htmlString += '</li>';
+        htmlString += '<h2>' + data.title + '</h2></a>';
+        htmlString += trim_str_to_n(data.content, 150);
+        htmlString += '<a href="' + data.url + '">' + 'Go to article</a>';
+        htmlString += '<p>Posted by ' + data.author + 'on ' + data.published_date + '</p>';
+        htmlString += '</a>';
+        htmlString += '</div>';
 
         newElementsHtml.push(htmlString);
     });
