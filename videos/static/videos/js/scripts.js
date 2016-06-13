c2f1cb8d9df24100e31ce7bb387df70eb88d2193
Michal Graczykowski <graczykowskimichal@gmail.com>
2016-06-13 15:18:10 +0200

    minor style updates

--- videos/static/videos/js/scripts.js
+++ videos/static/videos/js/scripts.js
@@ -10,18 +10,31 @@ function appendElements(newData) {
         htmlString += '<a href="' + data.url + '"><h2 class="title">' + data.title + '</h2></a>';
         htmlString += '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + data.video_url +
             '" frameborder="0" allowfullscreen></iframe>';
+
+        /* Updating likes part */
+        var upvote_class = 'upvote-button fa fa-thumbs-up fa-lg';
+        var downvote_class = 'downvote-button fa fa-thumbs-down fa-lg';
+
+        console.log(data.voting_status);
+        if (data.voting_status == 'upvoted') {
+            upvote_class += ' clicked';
+        }
+        if (data.voting_status == 'downvoted') {
+            downvote_class = ' clicked';
+        }
+
         htmlString += '<div class="video_buttons row">';
-        htmlString += '<button class="upvote-button" name="' + data.slug + '" id="upbtn_' + data.slug + '"' +
-            ' data-on-click-action="upvote">';
-        htmlString += '<i class="fa fa-thumbs-up fa-lg"></i>';
+        htmlString += '<i class="' + upvote_class +'" name="' + data.slug + '" id="upbtn_' + data.slug + '"' +
+            ' data-on-click-action="upvote"></i>';
         htmlString += '<section id="upvote_count_' + data.slug + '">' + data.up_votes + '</section>';
-        htmlString += '</button>';
-        htmlString += '<button class="downvote-button" name="' + data.slug + '" id="dwnbtn_'+ data.slug + '"' +
-                                'data-on-click-action="downvote">';
-        htmlString += '<i class="fa fa-thumbs-down fa-lg"></i>';
+
+        htmlString += '<i class="' + downvote_class + '" name="' + data.slug + '" id="dwnbtn_'+ data.slug + '"' +
+                                'data-on-click-action="downvote"></i>';
         htmlString += '<section id="downvote_count_'+ data.slug +'">' + data.down_votes +'</section>';
-        htmlString += '</button>';
         htmlString += '</div>';
+        /* End of updating likes */
+
+
         htmlString += '</li>';
         newElementsHtml.push(htmlString);
     });
